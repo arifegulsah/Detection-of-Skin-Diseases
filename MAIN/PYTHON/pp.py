55463f00c66af4792a945c45134f8c8f2fa73a3f
Zeynep <zesudegul@gmail.com>
2022-06-13 23:47:19 +0300

    s

--- MAIN/PYTHON/pp.py
+++ MAIN/PYTHON/pp.py
@@ -186,7 +186,7 @@ from tensorflow.keras.optimizers import SGD
 
 
 model = Sequential()
-model.add(Conv2D(256,(3,3),padding="same", activation="relu", input_shape=((128, 151, 3))))
+model.add(Conv2D(256,(3,3),padding="same", activation="relu", input_shape=((64, 76, 3))))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(BatchNormalization())
 
@@ -211,7 +211,7 @@ model.add(Dense(256,activation="relu"))
 #model.add(Dropout(0.2))
 model.add(Dense(128,activation="relu"))
 #model.add(Dropout(0.4))
-#model.add(Dense(64,activation="relu"))
+model.add(Dense(64,activation="relu"))
 model.add(Dropout(0.3))
 
 model.add(Dense(4, activation="softmax"))
@@ -232,11 +232,11 @@ model.compile(optimizer=SGD(lr=0.000001, momentum=0.09), loss='binary_crossentro
 
 history = model.fit(Xtrain, 
                 ytrain,
-                batch_size=16, 
+                batch_size=50, 
                 #validation_data=(Xtest, ytest),
                 shuffle=True,
                 verbose=1,
-                epochs=50,
+                epochs=20,
                 use_multiprocessing = True)
 
 pd.DataFrame(history.history).plot(figsize=(8,5))
